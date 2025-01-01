565510c18ecf7bd7b98a39a7fc2c2e07bea5e9f6
Nexis Foundation <nexisfoundation@Nexiss-MacBook-Pro-2.local>
2025-01-01 03:14:56 -0500

    Add root route redirect to game page

--- front-end/src/routes/MainRouter.jsx
+++ front-end/src/routes/MainRouter.jsx
@@ -1,11 +1,13 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import GameRouter from "./GameRouer";
 import Error404 from "../pages/error/Error404";
+
 function MainRouter() {
   return (
     <BrowserRouter basename="/">
       <Routes>
+        <Route path="/" element={<Navigate to="/game" replace />} />
         {GameRouter}
         <Route path="*" element={<Error404 />} />
       </Routes>
